b21630525cc9d0b07c136098ce12396119d94d5f
Wei Lai <wlai@liveperson.com>
2022-10-07 20:44:14 -0400

    color module works ith index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-"use strict";
+// "use strict";
 
-console.log('hello world');
+//const message = require("./message");
+const color = require("./color");
+
+// console.log(message.hello() + getBlue().name);
+//console.log(message.hello());
+console.log(color.getBlue().name);
